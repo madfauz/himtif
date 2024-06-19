26408ff0c6059bac01f659d3744c4eb86da13d6c
Ahmad Fauzi <ahmadfauzi8341@gmail.com>
2024-06-19 14:33:40 +0700

    edit_persiapan_diesnatalis_2

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,33 +15,45 @@ function HomepageHeader() {
   useEffect(() => {
     AOS.init();
   }, []);
+
   const { siteConfig } = useDocusaurusContext();
   return (
     <BrowserOnly>
       {() => (
         <header className={clsx("hero hero--primary", styles.heroBanner)}>
           <div className={styles.header_container}>
-            <h1 style={{ color: "white" }} className={styles.hero_title}>
+            <h1
+              style={{ color: "white" }}
+              className={styles.hero_title}
+              data-aos="fade-down"
+            >
               {siteConfig.title}
             </h1>
-            <p style={{ color: "white" }} className={styles.hero_subtitle}>
+            <p
+              style={{ color: "white" }}
+              className={styles.hero_subtitle}
+              data-aos="fade-up"
+            >
               {siteConfig.tagline}
             </p>
             <img
               className={styles.heroImage}
               src={useBaseUrl("/img/logo-himtif.png")}
               alt="HIMTIF"
+              data-aos="flip-up"
             />
             <div className={styles.buttons}>
               <Link
                 className={clsx("button button--secondary button--lg")}
                 to="#bph"
+                data-aos="fade-up"
               >
                 Badan Pengurus Harian
               </Link>
               <Link
                 className={clsx("button button--secondary button--lg")}
                 to={useBaseUrl("/docs/belati/intro-belati")}
+                data-aos="fade-up"
               >
                 Belati
               </Link>
